7c70be8abbf8c36ee944f344a209d2b9efce0c97
Maksym <maksym.myronov2018@gmail.com>
2023-04-11 19:29:50 +0300

    test

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -21,12 +21,7 @@ const vanElements = vans.map(van => (
 
 
   return (
-    <div className="van-list-container">
-      <h1>Explore our van options</h1>
-      <div className="van-list">
-        {vanElements}
-      </div>
-    </div>
+    <></>
   )
 }
 
