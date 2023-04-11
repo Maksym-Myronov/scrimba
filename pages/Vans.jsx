69aec4ca9beb2e4f4ccf5c5e997df42a69745a8d
Maksym <maksym.myronov2018@gmail.com>
2023-04-11 19:02:57 +0300

    fetch

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 export default function Vans() {
+  React.useEffect(() => {
+    fetch("/api/vans")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
+
   return (
     <h1>Vans page goes here 🚐</h1>
   )
