ac718cbae3cef05ff29b1788838f502df30b8377
Maksym <maksym.myronov2018@gmail.com>
2023-04-11 18:37:59 +0300

    add new file

--- index.jsx
+++ index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import "./server";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+import "./server"
+
 
 function App() {
   return (
